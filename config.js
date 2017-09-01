bec2a9e84700b2719a370466518e4899c7546bc2
Mael Wyssbrod <mael@ubuntu.media.int>
2017-09-01 11:26:02 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+(function (cloudStack) {
+  cloudStack.plugins.amystaPlugin.config = {
+    title: 'Amysta Billing',
+    desc: 'Open Amysta in a new tab.',
+    externalLink: 'http://www.swisstxt.ch',
+    authorName: 'Swiss TXT',
+    authorEmail: 'support@swisstxt.ch'
+  };
+}(cloudStack));
