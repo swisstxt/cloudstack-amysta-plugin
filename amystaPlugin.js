0b1e682b6c1ba8fc7f7f606b7f10a952b9b42e5e
sonofthechief <mael.wyssbrod@swisstxt.ch>
2017-09-05 09:22:19 +0200

    Parameterized

--- amystaPlugin.js
+++ amystaPlugin.js
@@ -7,13 +7,9 @@
         return true;
       },
       show: function() {
-        var amysta_domain = 'https://ma-cloud.swisstxt.ch';
+        var amysta_domain = "https://" + document.location.hostname;
         var amysta_path = '/billing';
-	var session_id = $.cookie("JSESSIONID");
-        var amysta_url = amysta_domain + amysta_path + '?jsessionid=' + session_id /* + '&sessionkey=' + g_sessionKey;*/
-	console.log( $.cookie("JSESSIONID") );
-	console.log(amysta_url);
-	console.log(document.cookie);
+        var amysta_url = amysta_domain + amysta_path;
 
 	window.open(amysta_url, "_blank");
         
